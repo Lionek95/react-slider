d499e4fe56e6097dbf8af2b73227cb0f1530688c
Katarzyna Jakubiec <kate_1995@tlen.pl>
2022-03-22 11:56:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { Navigation, Pagination, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import './App.css';
 
-function App() {
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import React, { useState, useEffect } from "react";
+const firstUrl = "https://avatars.githubusercontent.com/u/810438?v=4";
+//"https://api.github.com/users/gaearon";
+const secondUrl = "https://avatars.githubusercontent.com/u/3624098?v=4";
+//"https://api.github.com/users/acdlite";
+const thirdUrl = "https://avatars.githubusercontent.com/u/499550?v=4";
+//"https://api.github.com/users/yyx990803";
+const forthUrl = "https://avatars.githubusercontent.com/u/36901?v=4";
+//"https://api.github.com/users/unclebob";
+const fifthUrl = "https://avatars.githubusercontent.com/u/120859?v=4";
+//"https://api.github.com/users/martinfowler";
+
+export default function Slider () {
+
+  const [img, setImg] = useState();
+
+  const firstFetchImage = async () => {
+    const res = await fetch(firstUrl);
+    const imageBlob = await res.blob();
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    setImg(imageObjectURL);
+  };
+
+  useEffect(() => {
+    firstFetchImage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="Slider">
+        <h1><img src={require('./GitHub-logo.png')} height="48" /> Avatars Slider</h1>
+        <Swiper
+          modules={[Navigation, Pagination, A11y]}
+          spaceBetween={0}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <SwiperSlide><img src={img} alt="image" />
+          </SwiperSlide>
+          <SwiperSlide><img src={secondUrl} alt="image" />
+          </SwiperSlide>
+          <SwiperSlide><img src={thirdUrl} alt="image" />
+          </SwiperSlide>
+          <SwiperSlide><img src={forthUrl} alt="image" />
+          </SwiperSlide>
+          <SwiperSlide><img src={fifthUrl} alt="image" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      );
+    </>
   );
 }
-
-export default App;
